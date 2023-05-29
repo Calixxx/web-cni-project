6486acb33d85ddb33b3c90b4e970d93d22384e6e
Calixxx <calixmonnet@gmail.com>
2023-05-29 18:02:32 +0000

    administration - home & status

---
+++ frontend/js/script.js
@@ -0,0 +1,19 @@
+// (function () {
+//   "use strict";
+
+//   window.onscroll = function () {
+//     const header = document.querySelector(".header");
+//     const menuMain = document.querySelector(".content");
+
+//     console.log(window.pageYOffset)
+
+//     if (window.pageYOffset > header.offsetTop) {
+//       menuMain.classList.add("sticky-content");
+//     } else {
+//       menuMain.classList.remove("sticky-content");
+//     }
+//   };
+
+// })();
+
+  
